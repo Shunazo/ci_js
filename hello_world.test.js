c42a9e9c9629b5e726a61bb756faac2c4b951bbe
Shunazo <abeljunior0907@gmail.com>
2025-02-14 19:40:19 -0400

    test passed

--- hello_world.test.js
+++ hello_world.test.js
@@ -1,2 +1,5 @@
-const helloWorld = require('../hello_world.js')
+const helloWorld = require("./hello_world");
 
+test("Debe devolver 'Hello World!'", () => {
+    expect(helloWorld()).toBe("Hello World!");
+});
